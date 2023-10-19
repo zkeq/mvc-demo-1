49d386df1fe23a9610310ba8c3e39bd1b6b4562f
Zkeq <admin@icodeq.com>
2023-10-19 14:35:25 +0800

    初始化 完成4个功能

---
+++ src/app3.js
@@ -0,0 +1,8 @@
+import $ from "jquery"
+import "./app3.css"
+
+const $square = $("#app3 #square");
+
+$square.on("click", ()=> {
+    $square.toggleClass("active");
+})
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "./reset.css"
+import "./global.css" // css import 的性能比较低
+import "./app1.js"
+import "./app2.js"
+import "./app3.js"
+import "./app4.js"
